ef80ec5c77b83d88e2adfd3aa8ba652cea804911
local_j <jb@isiteletrade.com>
2015-05-19 17:45:34 +0800

    updated plugin

--- scrollAnimate.js
+++ scrollAnimate.js
@@ -5,23 +5,31 @@
 
         // scrollObj Class
         var scrollObj = function(elem){
-        
-            var obj = this,
-                targetId = elem.attr('href'),
-                targetElem = $(targetId),
-                targetTop = targetElem.offset().top,
-                speed = (Math.ceil(targetTop/1000))*500 ;
-            
-            elem.click(function(e){
-                e.preventDefault();
-                $('html,body').animate({
-                    scrollTop : targetTop
-                },speed,'easeInOutQuint');
-            });
+
+            // create object if element has an href attribute, restricting it to anchor tags
+            if ( elem.attr('href') )
+            {
+                var obj = this,
+                    targetId = elem.attr('href'),
+                    targetElem = $(targetId),
+                    targetTop = targetElem.offset().top,
+                    speed = (Math.ceil(targetTop/1000))*500 ;
+                
+                elem.click(function(e){
+                    e.preventDefault();
+                    $('html,body').animate({
+                        scrollTop : targetTop
+                    },speed,'easeInOutQuint');
+                });
+            }
+            else
+            {
+                return null
+            }
             
         }        
         
-        return this.each(function () {
+        return this.each(function () {            
             var scrollObjIns = new scrollObj($(this));
         });     
         
